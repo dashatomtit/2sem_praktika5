bd96e2794246a8721def3e06968ec78ee3954b08
Дарья <@LAPTOP-O9FJ6HUI>
2024-06-03 15:17:09 +0300

    Добавьте файлы проекта.

---
+++ WebAppTestData/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTestData.Controllers;
+
+public class AccountController : Controller
+{
+    // GET
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
---
+++ WebAppTestData/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded',function (){
+    let itemAccount = document.getElementsByClassName("account");
+    console.log(itemAccount);
+    itemAccount.textContent = 'Login in';
+    console.log(itemAccount);
+    // let buttonSignIn = document.getElementsByClassName("sign_in");
+    //
+    // buttonSignIn.onclick = signIn();
+    function signIn() {
+        itemAccount.textContent = 'Login out';
+        console.log(itemAccount);
+    }
+    
+},false);
